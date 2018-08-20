143c50b865bed9f4cf89010ec31a44d4617763d1
Simon Bull <simon.bull.84@googlemail.com>
2018-08-20 13:58:20 +0100

    Add endpoint to add identifier to user service

--- src/app/users/index.js
+++ src/app/users/index.js
@@ -7,6 +7,7 @@ const listUserServices = require('./listUserServices');
 const getSingleUserService = require('./getSingleUserService');
 const addServiceToUser = require('./addServiceToUser');
 const removeServiceFromUser = require('./removeServiceFromUser');
+const addServiceIdentifierToUser = require('./addServiceIdentifierToUser');
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ const buildArea = () => {
   router.get('/:uid/services/:sid/organisations/:oid', asyncWrapper(getSingleUserService)); // Duplicate for /:uid/services/:sid when org becomes optional
   router.put('/:uid/services/:sid/organisations/:oid', asyncWrapper(addServiceToUser)); // Duplicate for /:uid/services/:sid when org becomes optional
   router.delete('/:uid/services/:sid/organisations/:oid', asyncWrapper(removeServiceFromUser)); // Duplicate for /:uid/services/:sid when org becomes optional
+  router.put('/:uid/services/:sid/organisations/:oid/identifiers/:idkey', asyncWrapper(addServiceIdentifierToUser)); // Duplicate for /:uid/services/:sid/identifiers when org becomes optional
 
   return router;
 };
