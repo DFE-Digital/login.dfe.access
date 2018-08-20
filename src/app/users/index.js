d3d3d7b090fbcf895259b4cd63951799e8470092
Simon Bull <simon.bull.84@googlemail.com>
2018-08-20 10:28:15 +0100

    Add endpoint to delete user service mapping

--- src/app/users/index.js
+++ src/app/users/index.js
@@ -5,13 +5,14 @@ const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const listUserServices = require('./listUserServices');
 const addServiceToUser = require('./addServiceToUser');
+const removeServiceFromUser = require('./removeServiceFromUser');
 
 const router = express.Router();
 
 const buildArea = () => {
   router.get('/:uid/services', asyncWrapper(listUserServices));
   router.put('/:uid/services/:sid', asyncWrapper(addServiceToUser));
-  // router.delete('/:uid/services/:sid', asyncWrapper(stubAction));
+  router.delete('/:uid/services/:sid', asyncWrapper(removeServiceFromUser));
 
   return router;
 };
