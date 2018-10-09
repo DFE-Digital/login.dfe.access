16fdf421b4eaa3469da6366411e6924e509161f5
Simon Bull <simon.bull.84@googlemail.com>
2018-10-09 15:28:59 +0100

    Check identifier usage when adding to invitation

--- src/app/invitations/addServiceToInvitation.js
+++ src/app/invitations/addServiceToInvitation.js
@@ -1,13 +1,14 @@
 const logger = require('./../../infrastructure/logger');
-const { addInvitationService, addInvitationServiceIdentifier, removeAllInvitationServiceIdentifiers } = require('./../../infrastructure/data');
+const { addInvitationService, addInvitationServiceIdentifier, removeAllInvitationServiceIdentifiers, getUserOfServiceIdentifier } = require('./../../infrastructure/data');
 
-const parseAndValidateRequest = (req) => {
+const parseAndValidateRequest = async (req) => {
   const model = {
     iid: req.params.iid,
     sid: req.params.sid,
     oid: req.params.oid,
     identifiers: req.body.identifiers || [],
     errors: [],
+    errorStatus: 400,
   };
 
   if (!model.oid) {
@@ -18,12 +19,17 @@ const parseAndValidateRequest = (req) => {
     model.errors.push('Identifiers must be an array');
   } else {
     let allItemsOk = true;
-    model.identifiers.forEach((item) => {
+    for (let i = 0; i < model.identifiers.length; i += 1) {
+      const item = model.identifiers[i];
+
       const keys = Object.keys(item);
       if (!keys.find(x => x === 'key') || !keys.find(x => x === 'value')) {
         allItemsOk = false;
+      } else if (await getUserOfServiceIdentifier(model.sid, item.key, item.value)) {
+        model.errors.push(`Identifier ${item.key} already in use`);
+        model.errorStatus = 409;
       }
-    });
+    }
     if (!allItemsOk) {
       model.errors.push('Identifiers items must contain key and value');
     }
@@ -34,13 +40,13 @@ const parseAndValidateRequest = (req) => {
 
 const addServiceToInvitation = async (req, res) => {
   const correlationId = req.correlationId;
-  const model = parseAndValidateRequest(req);
+  const model = await parseAndValidateRequest(req);
   const { iid, oid, sid, identifiers } = model;
 
   logger.info(`Adding service ${sid} with org ${oid} to invitation ${iid} (correlation id: ${correlationId})`, { correlationId });
   try {
     if (model.errors.length > 0) {
-      return res.status(400).send({ details: model.errors });
+      return res.status(model.errorStatus).send({ details: model.errors });
     }
 
     await addInvitationService(iid, sid, oid);
