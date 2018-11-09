b6f8fce8fd92943121a317e404912dfd2e0cb0ed
Simon Bull <simon.bull.84@googlemail.com>
2018-11-09 14:57:53 +0000

    Store roles when adding user to service

---
+++ src/infrastructure/data/organisationsRepository/invitationServiceRoles.js
@@ -0,0 +1,40 @@
+const { default: Sequelize, Op } = require('sequelize').default;
+
+const define = (db, schema) => {
+  return db.define('invitation_service_roles', {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
+    invitation_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    organisation_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    service_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    role_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+  }, {
+    timestamps: true,
+    tableName: 'invitation_service_roles',
+    schema,
+  });
+};
+
+const extend = () => {
+};
+
+module.exports = {
+  name: 'inivitationServiceRoles',
+  define,
+  extend,
+};
