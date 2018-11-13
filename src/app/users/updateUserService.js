fa6a37e88163ae16ee4fba932703a7af6f635150
Simon Bull <simon.bull.84@googlemail.com>
2018-11-13 09:32:37 +0000

    Add patch user service endpoint

---
+++ src/app/users/updateUserService.js
@@ -0,0 +1,88 @@
+const logger = require('./../../infrastructure/logger');
+const { getUserService, addUserServiceIdentifier, removeAllUserServiceIdentifiers, getServiceRoles, removeAllUserServiceRoles, addUserServiceRole } = require('./../../infrastructure/data');
+
+const parseAndValidateRequest = async (req) => {
+  const model = {
+    uid: req.params.uid,
+    sid: req.params.sid,
+    oid: req.params.oid,
+    identifiers: req.body.identifiers || [],
+    roles: req.body.roles || [],
+    errors: [],
+  };
+
+  if (!model.oid) {
+    model.errors.push('Must specify organisation');
+  }
+
+  if (!(model.identifiers instanceof Array)) {
+    model.errors.push('Identifiers must be an array');
+  } else {
+    let allItemsOk = true;
+    model.identifiers.forEach((item) => {
+      const keys = Object.keys(item);
+      if (!keys.find(x => x === 'key') || !keys.find(x => x === 'value')) {
+        allItemsOk = false;
+      }
+    });
+    if (!allItemsOk) {
+      model.errors.push('Identifiers items must contain key and value');
+    }
+  }
+
+  if (!(model.roles instanceof Array)) {
+    model.errors.push('Roles must be an array');
+  } else {
+    const availableRolesForService = await getServiceRoles(model.sid);
+    model.roles.forEach((roleId) => {
+      const safeRoleId = (roleId || '').toLowerCase();
+      if (!availableRolesForService.find(x => x.id.toLowerCase() === safeRoleId.toLowerCase())) {
+        model.errors.push(`Role ${roleId} is not available for service ${model.sid}`);
+      }
+    });
+  }
+
+  return model;
+};
+
+const updateUserService = async (req, res) => {
+  const correlationId = req.correlationId;
+  const model = await parseAndValidateRequest(req);
+  const { uid, oid, sid, identifiers, roles } = model;
+
+  logger.info(`Updating service ${sid} with org ${oid} for user ${uid} (correlation id: ${correlationId})`, { correlationId });
+  try {
+    if (model.errors.length > 0) {
+      return res.status(400).send({ details: model.errors });
+    }
+
+    const userService = await getUserService(uid, sid, oid);
+    if (!userService) {
+      return res.status(404).send();
+    }
+
+    if (identifiers.length > 0) {
+      await removeAllUserServiceIdentifiers(uid, sid, oid);
+      for (let i = 0; i < identifiers.length; i += 1) {
+        await addUserServiceIdentifier(uid, sid, oid, identifiers[i].key, identifiers[i].value);
+      }
+    }
+
+    if (roles.length > 0) {
+      await removeAllUserServiceRoles(uid, sid, oid);
+      for (let i = 0; i < roles.length; i += 1) {
+        await addUserServiceRole(uid, sid, oid, roles[i]);
+      }
+    }
+
+    return res.status(202).send();
+  } catch (e) {
+    logger.error(`Error adding service ${sid} with org ${oid} to user ${uid} (correlation id: ${correlationId}) - ${e.message}`, {
+      correlationId,
+      stack: e.stack
+    });
+    throw e;
+  }
+};
+
+module.exports = updateUserService;
---
+++ test/app/users/updateUserService.test.js
@@ -0,0 +1,211 @@
+jest.mock('./../../../src/infrastructure/logger', () => require('./../../utils').mockLogger());
+jest.mock('./../../../src/infrastructure/data', () => ({
+  getUserService: jest.fn(),
+  addUserServiceIdentifier: jest.fn(),
+  removeAllUserServiceIdentifiers: jest.fn(),
+  getServiceRoles: jest.fn(),
+  removeAllUserServiceRoles: jest.fn(),
+  addUserServiceRole: jest.fn(),
+}));
+
+const { mockRequest, mockResponse } = require('./../../utils');
+const { getUserService, addUserServiceIdentifier, removeAllUserServiceIdentifiers, getServiceRoles, removeAllUserServiceRoles, addUserServiceRole } = require('./../../../src/infrastructure/data');
+const updateUserService = require('./../../../src/app/users/updateUserService');
+
+const uid = 'user1';
+const sid = 'service1';
+const oid = 'organisation1';
+const res = mockResponse();
+
+describe('When updating service of user', () => {
+  let req;
+
+  beforeEach(() => {
+    getUserService.mockReset().mockReturnValue({
+      userId: uid,
+      serviceId: sid,
+      organisationId: oid,
+      roles: [
+        { id: 'existing-role' },
+      ],
+      identifiers: [
+        { key: 'existing', value: 'identifier' },
+      ],
+    });
+    addUserServiceIdentifier.mockReset();
+    removeAllUserServiceIdentifiers.mockReset();
+    getServiceRoles.mockReset().mockReturnValue([
+      { id: 'role1' },
+      { id: 'role3' },
+    ]);
+    removeAllUserServiceRoles.mockReset();
+    addUserServiceRole.mockReset();
+
+    req = mockRequest({
+      params: {
+        uid,
+        sid,
+        oid,
+      },
+      body: {
+        identifiers: [
+          { key: 'some', value: 'thing' },
+          { key: 'something', value: 'else' },
+        ],
+        roles: [
+          'role1',
+          'role3',
+        ],
+      },
+    });
+    res.mockResetAll();
+  });
+
+  it('then it should get current user service', async () => {
+    await updateUserService(req, res);
+
+    expect(getUserService).toHaveBeenCalledTimes(1);
+    expect(getUserService).toHaveBeenCalledWith(uid, sid, oid);
+  });
+
+  it('then it should return 404 if no mapping already exists for user/service/org', async () => {
+    getUserService.mockReturnValue(undefined);
+
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('then it should remove existing identifiers if new identifiers specified', async () => {
+    await updateUserService(req, res);
+
+    expect(removeAllUserServiceIdentifiers).toHaveBeenCalledTimes(1);
+    expect(removeAllUserServiceIdentifiers).toHaveBeenCalledWith(uid, sid, oid);
+  });
+
+  it('then it should add identifiers if specified', async () => {
+    await updateUserService(req, res);
+
+    expect(addUserServiceIdentifier).toHaveBeenCalledTimes(2);
+    expect(addUserServiceIdentifier).toHaveBeenCalledWith(uid, sid, oid, 'some', 'thing');
+    expect(addUserServiceIdentifier).toHaveBeenCalledWith(uid, sid, oid, 'something', 'else');
+  });
+
+  it('then it should not attempt to remove or add identifiers if none specified', async () => {
+    req.body.identifiers = undefined;
+
+    await updateUserService(req, res);
+
+    expect(removeAllUserServiceIdentifiers).not.toHaveBeenCalled();
+    expect(addUserServiceIdentifier).not.toHaveBeenCalled();
+  });
+
+  it('then it should remove existing roles if new roles specified', async () => {
+    await updateUserService(req, res);
+
+    expect(removeAllUserServiceRoles).toHaveBeenCalledTimes(1);
+    expect(removeAllUserServiceRoles).toHaveBeenCalledWith(uid, sid, oid);
+  });
+
+  it('then it should add roles if specified', async () => {
+    await updateUserService(req, res);
+
+    expect(addUserServiceRole).toHaveBeenCalledTimes(2);
+    expect(addUserServiceRole).toHaveBeenCalledWith(uid, sid, oid, 'role1');
+    expect(addUserServiceRole).toHaveBeenCalledWith(uid, sid, oid, 'role3');
+  });
+
+  it('then it should not attempt to remove or add roles if none specified', async () => {
+    req.body.roles = undefined;
+
+    await updateUserService(req, res);
+
+    expect(removeAllUserServiceRoles).not.toHaveBeenCalled();
+    expect(addUserServiceRole).not.toHaveBeenCalled();
+  });
+
+  it('then it should return 202 response', async () => {
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(202);
+    expect(res.send).toHaveBeenCalledTimes(1);
+  });
+
+  it('then it should return 400 if identifiers specified but not array', async () => {
+    req.body.identifiers = 'not-an-array';
+
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send.mock.calls[0][0]).toEqual({
+      details: [
+        'Identifiers must be an array',
+      ],
+    });
+  });
+
+  it('then it should return 400 if identifiers specified and items does not have key', async () => {
+    req.body.identifiers = [{ value: 'thing' }];
+
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send.mock.calls[0][0]).toEqual({
+      details: [
+        'Identifiers items must contain key and value',
+      ],
+    });
+  });
+
+  it('then it should return 400 if identifiers specified and items does not have value', async () => {
+    req.body.identifiers = [{ key: 'some' }];
+
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send.mock.calls[0][0]).toEqual({
+      details: [
+        'Identifiers items must contain key and value',
+      ],
+    });
+  });
+
+  it('then it should return 400 if roles specified but not array', async () => {
+    req.body.roles = 'not-an-array';
+
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send.mock.calls[0][0]).toEqual({
+      details: [
+        'Roles must be an array',
+      ],
+    });
+  });
+
+  it('then it should return 400 if role not valid for service', async () => {
+    req.body.roles = ['role2'];
+
+    await updateUserService(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send.mock.calls[0][0]).toEqual({
+      details: [
+        'Role role2 is not available for service service1',
+      ],
+    });
+  });
+});
