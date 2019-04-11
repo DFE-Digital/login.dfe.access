81f3aeeaed924d39ca61b3131b007908dff0bb6a
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-04-11 12:19:49 +0100

    NSA-2085- revert commit

--- test/infrastructure/data/index.getPoliciesForService.test.js
+++ test/infrastructure/data/index.getPoliciesForService.test.js
@@ -6,29 +6,29 @@ const { getPoliciesForService } = require('./../../../src/infrastructure/data');
 const { Op } = require('sequelize');
 
 const sid = 'service-1';
-const policies = {
-  rows: [
-    mockPolicyEntity(),
-    mockPolicyEntity({
-      roles: [mockRoleEntity(), mockRoleEntity()],
-      conditions: [mockPolicyConditionEntity({ field: 'id' }), mockPolicyConditionEntity({ field: 'organisation.id' }), mockPolicyConditionEntity({ field: 'id' })],
-    }),
-  ]
-};
+const policies = [
+  mockPolicyEntity(),
+  mockPolicyEntity({
+    roles: [mockRoleEntity(), mockRoleEntity()],
+    conditions: [mockPolicyConditionEntity({ field: 'id' }), mockPolicyConditionEntity({ field: 'organisation.id' }), mockPolicyConditionEntity({ field: 'id' })],
+  }),
+];
 
 describe('When getting policies for service from the repository', () => {
   beforeEach(() => {
-    repository.policies.mockResetAll();
+    repository.mockResetAll();
 
-    repository.policies.findAndCountAll.mockReturnValue(policies);
+    repository.policies.findAll.mockReturnValue(policies);
 
-});
+    policies[0].mockResetAll();
+    policies[1].mockResetAll();
+  });
 
   it('then it should find all for applicationId', async () => {
-    await getPoliciesForService(sid, 1, 50);
+    await getPoliciesForService(sid);
 
-    expect(repository.policies.findAndCountAll).toHaveBeenCalledTimes(1);
-    expect(repository.policies.findAndCountAll.mock.calls[0][0]).toMatchObject({
+    expect(repository.policies.findAll).toHaveBeenCalledTimes(1);
+    expect(repository.policies.findAll.mock.calls[0][0]).toMatchObject({
       where: {
         applicationId: {
           [Op.eq]: sid,
@@ -38,60 +38,60 @@ describe('When getting policies for service from the repository', () => {
   });
 
   it('then it should order results by policy name', async () => {
-    await getPoliciesForService(sid, 1, 50);
+    await getPoliciesForService(sid);
 
-    expect(repository.policies.findAndCountAll).toHaveBeenCalledTimes(1);
-    expect(repository.policies.findAndCountAll.mock.calls[0][0]).toMatchObject({
+    expect(repository.policies.findAll).toHaveBeenCalledTimes(1);
+    expect(repository.policies.findAll.mock.calls[0][0]).toMatchObject({
       order: ['name'],
     });
   });
 
   it('then it should return policies mapped to model', async () => {
-    const actual = await getPoliciesForService(sid, 1, 50);
+    const actual = await getPoliciesForService(sid);
 
-    expect(actual.policies.length).toBe(2);
-    expect(actual.policies[0]).toEqual({
-      id: policies.rows[0].id,
-      name: policies.rows[0].name,
-      applicationId: policies.rows[0].applicationId,
+    expect(actual.length).toBe(2);
+    expect(actual[0]).toEqual({
+      id: policies[0].id,
+      name: policies[0].name,
+      applicationId: policies[0].applicationId,
       status: {
-        id: policies.rows[0].status,
+        id: policies[0].status,
       },
       conditions: [],
       roles: [],
     });
-    expect(actual.policies[1]).toEqual({
-      id: policies.rows[1].id,
-      name: policies.rows[1].name,
-      applicationId: policies.rows[1].applicationId,
+    expect(actual[1]).toEqual({
+      id: policies[1].id,
+      name: policies[1].name,
+      applicationId: policies[1].applicationId,
       status: {
-        id: policies.rows[1].status,
+        id: policies[1].status,
       },
       conditions: [
         {
-          field: policies.rows[1].conditions[0].field,
-          operator: policies.rows[1].conditions[0].operator,
-          value: [policies.rows[1].conditions[0].value, policies.rows[1].conditions[2].value],
+          field: policies[1].conditions[0].field,
+          operator: policies[1].conditions[0].operator,
+          value: [policies[1].conditions[0].value, policies[1].conditions[2].value],
         },
         {
-          field: policies.rows[1].conditions[1].field,
-          operator: policies.rows[1].conditions[1].operator,
-          value: [policies.rows[1].conditions[1].value],
+          field: policies[1].conditions[1].field,
+          operator: policies[1].conditions[1].operator,
+          value: [policies[1].conditions[1].value],
         },
       ],
       roles: [
         {
-          id: policies.rows[1].roles[0].id,
-          name: policies.rows[1].roles[0].name,
+          id: policies[1].roles[0].id,
+          name: policies[1].roles[0].name,
           status: {
-            id: policies.rows[1].roles[0].status,
+            id: policies[1].roles[0].status,
           },
         },
         {
-          id: policies.rows[1].roles[1].id,
-          name: policies.rows[1].roles[1].name,
+          id: policies[1].roles[1].id,
+          name: policies[1].roles[1].name,
           status: {
-            id: policies.rows[1].roles[1].status,
+            id: policies[1].roles[1].status,
           },
         }
       ],
