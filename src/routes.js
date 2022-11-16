0a06624bb7f9c235faffcb22060e121d606eaa09
Univer-lion-lab <rafael.tsiakkouris@methods.co.uk>
2022-11-16 09:27:47 +0000

    route & security restrictions gateway and health check

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,9 @@ const invitations = require('./app/invitations');
 const services = require('./app/services');
 
 const registerRoutes = (app) => {
-  app.use('/users', users());
-  app.use('/invitations', invitations());
-  app.use('/services', services());
+  app.use('/acc/users', users());
+  app.use('/acc/invitations', invitations());
+  app.use('/acc/services', services());
 };
 
 module.exports = registerRoutes;
