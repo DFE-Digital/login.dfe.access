d96a6fcbb15190dccad2b302366fa8a0531a8488
malikimran34 <malikimran21@icloud.com>
2023-11-03 15:34:26 +0000

    DSI-6196 Update helmet code to include custom domains

--- src/index.js
+++ src/index.js
@@ -20,6 +20,37 @@ app.use(helmet({
   },
 }));
 
+logger.info('set helmet policy defaults');
+
+// Setting helmet Content Security Policy
+const scriptSources = ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'localhost', '*.signin.education.gov.uk', 'https://code.jquery.com'];
+
+app.use(helmet.contentSecurityPolicy({
+  browserSniff: false,
+  setAllHeaders: false,
+  useDefaults: false,
+  directives: {
+    defaultSrc: ["'self'"],
+    childSrc: ["'none'"],
+    objectSrc: ["'none'"],
+    scriptSrc: scriptSources,
+    styleSrc: ["'self'", "'unsafe-inline'", 'localhost', '*.signin.education.gov.uk'],
+    imgSrc: ["'self'", 'data:', 'blob:', 'localhost', '*.signin.education.gov.uk'],
+    fontSrc: ["'self'", 'data:', '*.signin.education.gov.uk'],
+    connectSrc: ["'self'"],
+    formAction: ["'self'", '*'],
+  },
+}));
+
+logger.info('Set helmet filters');
+
+app.use(helmet.xssFilter());
+app.use(helmet.frameguard('false'));
+app.use(helmet.ieNoOpen());
+
+logger.info('helmet setup complete');
+
+
 if (config.hostingEnvironment.env !== 'dev') {
   app.set('trust proxy', 1);
 
