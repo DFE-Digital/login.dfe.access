859ad964d93fe11415e78c94e37d5ce13dda29a8
Adam Mann <Adam.Mann@education.gov.uk>
2025-04-08 15:42:35 +0100

    Tidy up code and write test

--- src/app/users/updateLastAccess.js
+++ src/app/users/updateLastAccess.js
@@ -1,6 +1,6 @@
-const logger = require('../../infrastructure/logger');
-const { updateUserServiceLastAccess } = require('../../infrastructure/data');
-const { notifyUserUpdated } = require('../../infrastructure/notifications');
+const logger = require("../../infrastructure/logger");
+const { updateUserServiceLastAccess } = require("../../infrastructure/data");
+const { notifyUserUpdated } = require("../../infrastructure/notifications");
 
 const parseAndValidateRequest = async (req) => {
   const model = {
@@ -10,13 +10,13 @@ const parseAndValidateRequest = async (req) => {
     errors: [],
   };
   if (!model.uid) {
-    model.errors.push('Must specify user');
+    model.errors.push("Must specify user");
   }
   if (!model.sid) {
-    model.errors.push('Must specify service');
+    model.errors.push("Must specify service");
   }
   if (!model.oid) {
-    model.errors.push('Must specify organisation');
+    model.errors.push("Must specify organisation");
   }
   return model;
 };
@@ -24,25 +24,37 @@ const parseAndValidateRequest = async (req) => {
 const updateLastAccess = async (req, res) => {
   const { correlationId } = req;
   const model = await parseAndValidateRequest(req);
-  const {
-    uid, oid, sid,
-  } = model;
+  const { uid, oid, sid } = model;
 
-  logger.info(`Setting last access for service ${sid} with org ${oid} for user ${uid} (correlation id: ${correlationId})`, { correlationId });
+  logger.info(
+    `Setting last access for service ${sid} with org ${oid} for user ${uid} (correlation id: ${correlationId})`,
+    { correlationId },
+  );
   try {
     if (model.errors.length > 0) {
       return res.status(400).send({ details: model.errors });
     }
 
-    await updateUserServiceLastAccess(uid, sid, oid);
+    const userServicesRecordId = await updateUserServiceLastAccess(
+      uid,
+      sid,
+      oid,
+    );
+    logger.info(
+      `A user_services record with id [${userServicesRecordId}] was updated (correlation id: ${correlationId})`,
+      { correlationId },
+    );
 
     notifyUserUpdated(uid);
     return res.status(202).send();
   } catch (e) {
-    logger.error(`Error updating last access for service ${sid} with org ${oid} to user ${uid} (correlation id: ${correlationId}) - ${e.message}`, {
-      correlationId,
-      stack: e.stack,
-    });
+    logger.error(
+      `Error updating last access for service ${sid} with org ${oid} to user ${uid} (correlation id: ${correlationId}) - ${e.message}`,
+      {
+        correlationId,
+        stack: e.stack,
+      },
+    );
     throw e;
   }
 };
