bb755d2fd131d1d58f5a575912b589167cb6b141
Adam Mann <manny160389@hotmail.com>
2025-07-18 11:31:20 +0100

    [NSA-9255] Add POST version of services endpoint (#133)
    * Add POST version of services endpoint
    
    * Add tests and refactor code
    
    * Rename function and add test

--- src/app/services/listUsersOfService.js
+++ src/app/services/listUsersOfService.js
@@ -1,13 +1,34 @@
 const { getUsersOfServicePaged } = require("./../../infrastructure/data");
 
-const getQueryStringValue = (req, key) => {
-  const qsKey = Object.keys(req.query).find(
+/**
+ * Gets a value from the request object by using the supplied key.  If the
+ * request has a POST method, then it will get it from the body.  If the
+ * request has a GET method then it will get it from the query
+ *
+ * @param {*} req The request object
+ * @param {String} key The key to get the value out of the body or query
+ */
+const getValueFromRequest = (req, key) => {
+  let paramsSource = req.method === "POST" ? req.body : req.query;
+  const qsKey = Object.keys(paramsSource).find(
     (x) => x.toLowerCase() === key.toLowerCase(),
   );
-  return qsKey ? req.query[qsKey] : undefined;
+  return qsKey ? paramsSource[qsKey] : undefined;
 };
-const getQueryStringIntValue = (req, key, defaultValue = 0) => {
-  const value = getQueryStringValue(req, key);
+
+/**
+ * Gets a value from the request object by using the supplied key. The value will be
+ * converted into a number.  If the value cannot be converted into a number, an
+ * exception will be raised.
+ * If the request has a POST method, then it will get it from the body.  If the
+ * request has a GET method then it will get it from the query
+ *
+ * @param {*} req The request object
+ * @param {String} key The key to get the value out of the body or query
+ * @param {number} defaultValue - An optional default if the key is missing from the request
+ */
+const getIntValueFromRequest = (req, key, defaultValue = 0) => {
+  const value = getValueFromRequest(req, key);
   if (!value) {
     return defaultValue;
   }
@@ -18,16 +39,17 @@ const getQueryStringIntValue = (req, key, defaultValue = 0) => {
   }
   return int;
 };
+
 const getFilters = (req) => {
   let filters = undefined;
 
-  const idkey = getQueryStringValue(req, "filteridkey");
+  const idkey = getValueFromRequest(req, "filteridkey");
   if (idkey) {
     filters = filters || {};
     filters.idkey = idkey;
   }
 
-  const idvalue = getQueryStringValue(req, "filteridvalue");
+  const idvalue = getValueFromRequest(req, "filteridvalue");
   if (idvalue) {
     filters = filters || {};
     filters.idvalue = idvalue;
@@ -37,13 +59,15 @@ const getFilters = (req) => {
 };
 
 const listUsersOfService = async (req, res) => {
-  const page = getQueryStringIntValue(req, "page", 1);
-  const pageSize = getQueryStringIntValue(req, "pageSize", 50);
-  let userIds = getQueryStringValue(req, "userIds");
+  const page = getIntValueFromRequest(req, "page", 1);
+  const pageSize = getIntValueFromRequest(req, "pageSize", 50);
 
-  // If params are in the form of value1,value2,value3,etc then convert into an array
-  if (userIds && !Array.isArray(userIds)) {
-    userIds = userIds.split(",");
+  let userIds = undefined;
+
+  if (req.method === "POST") {
+    // userIds are in POST only, because if more than 119 ids are provided as a query
+    // parameter, it will return a 431 status error.
+    userIds = getValueFromRequest(req, "userIds");
   }
   const filters = getFilters(req);
 
