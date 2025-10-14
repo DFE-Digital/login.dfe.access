515a8251892f47d25a689b1fbcd6e83c199c4ea1
Alex Platt <alex.platt@education.gov.uk>
2025-10-14 13:02:00 +0100

    [NSA-9359] Update createServiceRole return value

--- src/app/services/createRoleOfService.js
+++ src/app/services/createRoleOfService.js
@@ -1,7 +1,6 @@
 const { createServiceRole } = require("../../infrastructure/data");
 
 const createRoleOfService = async (req, res) => {
-  // create row in roles table
   const { sid, roleName, roleCode } = req.body;
 
   if (!sid || !roleName || !roleCode) {
