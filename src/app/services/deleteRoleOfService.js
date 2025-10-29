48345d932c05c01740d4c25d356a01b7755e268b
Alex Platt <alex.platt@education.gov.uk>
2025-10-29 12:56:45 +0000

    [NSA-9285] Remove unneeded comments

--- src/app/services/deleteRoleOfService.js
+++ src/app/services/deleteRoleOfService.js
@@ -7,31 +7,16 @@ const {
 const deleteRoleOfService = async (req, res) => {
   const { correlationId } = req;
   const { sid, rid } = req.params;
-  const model = req.body;
-  console.log("req.body: ", model);
-
-  // console.log("code: ", code);
-  // console.log("rid: ", rid);
-  // console.log("sid: ", sid);
 
   logger.info(`Deleting role ${rid} for service ${sid}`, { correlationId });
-  // return res.status(200).send(); //! remove
 
   try {
-    const existingRole = await getServiceRole(model.appId, model.roleCode);
-    console.log("existingRole: ", existingRole);
-    // if (
-    //   !existingPolicy ||
-    //   existingPolicy.applicationId.toLowerCase() !== sid.toLowerCase()
-    // ) {
-    if (!existingRole) {
+    const existingRole = await getServiceRole(sid, rid);
+    if (!existingRole || existingRole.id.toLowerCase() !== rid.toLowerCase()) {
       return res.status(404).send();
     }
-    // appId, roleName, roleCode
-    await deleteServiceRole(model);
-    //   await deletePolicyConditions(pid);
-    //   await deletePolicyRoles(pid);
-    //   await deletePolicy(pid);
+
+    await deleteServiceRole(sid, rid);
 
     return res.status(204).send();
   } catch (e) {
