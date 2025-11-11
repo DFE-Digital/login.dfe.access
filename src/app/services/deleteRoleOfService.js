6a70cb44e811533181c9d99c0f1ee376cb5e3e6d
Alex Platt <alex.platt@education.gov.uk>
2025-11-11 08:37:26 +0000

    [NSA-9452] Add tests

--- src/app/services/deleteRoleOfService.js
+++ src/app/services/deleteRoleOfService.js
@@ -5,19 +5,34 @@ const {
 } = require("../../infrastructure/data");
 
 const deleteRoleOfService = async (req, res) => {
-  console.log("deleteRoleOfService deleteRoleOfService deleteRoleOfService");
   const { correlationId } = req;
   const { sid, rid } = req.params;
-
-  logger.info(`Deleting role ${rid} for service ${sid}`, { correlationId });
+  const code = req.query?.roleCode;
 
   try {
-    const existingRole = await getServiceRole(sid, rid);
-    if (!existingRole || existingRole.id.toLowerCase() !== rid.toLowerCase()) {
+    const existingRole = await getServiceRole(sid, code);
+
+    if (!existingRole) {
+      logger.info(
+        `Could not find requested role to be deleted for service ${sid}`,
+        {
+          correlationId,
+        },
+      );
+      return res.status(404).send();
+    }
+    if (existingRole && existingRole.id.toLowerCase() !== rid.toLowerCase()) {
+      logger.info(
+        `Role deletion failed. Role id mismatch: [Request: ${rid.toLowerCase()}] - [Existing: ${existingRole.id.toLowerCase()}]`,
+        {
+          correlationId,
+        },
+      );
       return res.status(404).send();
     }
 
-    await deleteServiceRole(rid, sid);
+    logger.info(`Deleting role ${rid} for service ${sid}`, { correlationId });
+    await deleteServiceRole(sid, rid);
 
     return res.status(204).send();
   } catch (e) {
